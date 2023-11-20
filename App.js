3f08254fd488e94561b9bda6ca0c1d7594552727
mzruiz <matthew.z.ruiz@gmail.com>
2023-11-20 10:32:43 -0500

    Init

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF645C',
     alignItems: 'center',
     justifyContent: 'center',
   },
